2c9ad700218040d5741c60e38ce957583a258e6a
Halisson Julio Lopes Pereira <halissonjulio@gmail.com>
2023-03-15 12:42:23 -0300

    Comentários adicionados.

--- app/src/main/java/pereira/lopes/julio/app_01/MainActivity.java
+++ app/src/main/java/pereira/lopes/julio/app_01/MainActivity.java
@@ -11,17 +11,29 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //Método de selecionar o texto no evento de clique
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        //Seleciona o o layout da pasta resources
+
         setContentView(R.layout.activity_main);
+        //Cria o botão 'btnEnviar'
         Button btnEnviar = findViewById(R.id.btnEnviar);
+
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
+
+                //Retira o texto da variável 'etDigiteAqui' e converte para o tipo string
                 String texto = etDigiteAqui.getText().toString();
+
+                //Cria um Intent que serve para ativar a proxima Activity
                 Intent intent = new Intent(MainActivity.this, NextActivity.class);
                 intent.putExtra("texto", texto);
+
+                //Roda o Intent e troca de Activity
                 startActivity(intent);
             }
         });
--- app/src/main/java/pereira/lopes/julio/app_01/NextActivity.java
+++ app/src/main/java/pereira/lopes/julio/app_01/NextActivity.java
@@ -9,12 +9,22 @@ import android.widget.TextView;
 public class NextActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        //Recebe a intent da Activity anterior
         Intent i = getIntent();
+
+        //Texto
         String texto = i.getStringExtra("texto");
+
+        //Pega o id do campo para exibir o texto
         TextView tvExibir = findViewById(R.id.tvExibir);
+
+        //Define o texto a ser definido
         tvExibir.setText(texto);
+
     }
 }
