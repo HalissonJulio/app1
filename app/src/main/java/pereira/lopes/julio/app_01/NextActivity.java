d80f96c8bd7b0afc215083a014f491b002656e03
Halisson Julio Lopes Pereira <20211TIIMI0176@cefetes.br>
2023-03-15 12:13:39 -0300

    Initial commit

---
+++ app/src/main/java/pereira/lopes/julio/app_01/NextActivity.java
@@ -0,0 +1,20 @@
+package pereira.lopes.julio.app_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        Intent i = getIntent();
+        String texto = i.getStringExtra("texto");
+        TextView tvExibir = findViewById(R.id.tvExibir);
+        tvExibir.setText(texto);
+    }
+}
